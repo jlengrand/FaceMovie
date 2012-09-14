30929c4bf879c561eb85a82466d81892e5a85ac3
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-09-14 14:33:10 +0200

    Changes repo to ivolution

--- ivolution/util/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-	
-.. module:: FaceMovifier
-   :platform: Unix, Windows
-   :synopsis: This package aims at containing all libraries or external elements needed to create the FaceMovie
-
-.. moduleauthor:: Julien Lengrand-Lambert <jlengrand@gmail.com>
-
-"""
--- test/dirdialogtest.py
+++
@@ -1,8 +0,0 @@
-import wx
-
-
-app = wx.PySimpleApp()
-dialog = wx.DirDialog(None, "Choose a directory:",style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
-if dialog.ShowModal() == wx.ID_OK:
-    print dialog.GetPath()
-dialog.Destroy()
